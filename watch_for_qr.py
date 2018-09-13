7e6acfdb4ca74d2b5eab8d3c26163ef055d1f38d
Daniel M. Drucker <dmd@3e.org>
2018-09-12 21:00:01 -0400

    first working version

--- watch_for_qr.py
+++ watch_for_qr.py
@@ -1,29 +1,33 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import zbar
 from PIL import Image
 import cv2
 from time import time
+import musicpd
 
 def main():
+    music = musicpd.MPDClient()
+    music.connect('localhost', 6600)
+    music.update()
 
     lastdecoded = ""
     capture = cv2.VideoCapture(0)
-    scanner = zbar.ImageScanner()
+    scanner = zbar.Scanner()
 
     while True:
         ret, frame = capture.read()
-        # cv2.imshow('Current', frame)
+#        cv2.imshow('Current', frame)
 
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-        width, height = image.size
-        zbar_image = zbar.Image(width, height, 'Y800', image.tobytes())
-        scanner.scan(zbar_image)
 
-        for decoded in zbar_image:
+        for decoded in scanner.scan(image):
             if decoded.data != lastdecoded: # or more than x secs passed?
                 lastdecoded = decoded.data
-                print(str(time()) + " " + decoded.data)
+                print(decoded.data.decode("utf-8"))
+                music.clear()
+                music.load(decoded.data.decode("utf-8") + ".m3u")
+                music.play()
 
 
 if __name__ == "__main__":
