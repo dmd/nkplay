b62787c2d01a8a9932604b7c2baaad59e75d73d5
Daniel M. Drucker <dmd@3e.org>
2018-09-14 16:40:15 -0400

    screw it, it's a lot simpler if the only rule is MUST BE NEW

--- watch_for_qr.py
+++ watch_for_qr.py
@@ -25,18 +25,9 @@ def main():
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
         for decoded in scanner.scan(image):
-            # successfully acquired a QR code!
-            lastdecodedtime = time()
             ddd = decoded.data.decode("utf-8")
 
-            # we should take action on the QR code if any of:
-            #   - it is different from the last one we TOOK ACTION on
-            #   - we are not currently PLAYing
-            #   - it has been more than 15 seconds since the last time we saw a QR code
-            #     (this means it should be OK to LEAVE a code in view, as lastdecodedtime gets updated
-            #      even if no ACTION gets taken on it)
-
-            if ddd != lastdecoded or music.status()['state'] != 'play' or time() - lastdecodedtime > 15
+            if ddd != lastdecoded
                 lastdecoded = ddd
 
                 if ddd == 'STOP':
