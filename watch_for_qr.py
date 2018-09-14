c33745d60bf689ff5d34881684a943166d8c0e17
Daniel M. Drucker <ddrucker@abinitio.com>
2018-09-14 16:19:07 -0400

    when to take action on a seen code

--- watch_for_qr.py
+++ watch_for_qr.py
@@ -25,10 +25,19 @@ def main():
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
         for decoded in scanner.scan(image):
+            # successfully acquired a QR code!
+            lastdecodedtime = time()
             ddd = decoded.data.decode("utf-8")
-            if ddd != lastdecoded or time() - lastdecodedtime > 30:
+
+            # we should take action on the QR code if any of:
+            #   - it is different from the last one we TOOK ACTION on
+            #   - we are not currently PLAYing
+            #   - it has been more than 15 seconds since the last time we saw a QR code
+            #     (this means it should be OK to LEAVE a code in view, as lastdecodedtime gets updated
+            #      even if no ACTION gets taken on it)
+
+            if ddd != lastdecoded or music.status()['state'] != 'play' or time() - lastdecodedtime > 15
                 lastdecoded = ddd
-                lastdecodedtime = time()
 
                 if ddd == 'STOP':
                     music.clear()
@@ -37,6 +46,7 @@ def main():
                     try:
                         music.clear()
                         music.load(ddd + ".m3u")
+                        music.consume(1)
                         music.play()
                         print('Now playing: ' + ddd)
                     except:
